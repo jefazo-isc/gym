a287fc9d50e849432dd465882d2b08707c3b3a68
yahir <pouputopou4@gmail.com>
2025-05-09 11:00:03 -0600

    [Yahir] video seguro pipes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CardsPrincipalComponent } from '../../cards-principal/cards-principal.component';
 import { CarruselPrincipalComponent } from '../../carrusel-principal/carrusel-principal.component';
+import { VideoComponent} from '../video/video.component';
 import { UbicacionesPrincipalComponent } from '../../ubicaciones-principal/ubicaciones-principal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardsPrincipalComponent, CarruselPrincipalComponent,UbicacionesPrincipalComponent],
+  imports: [CardsPrincipalComponent, CarruselPrincipalComponent,VideoComponent,UbicacionesPrincipalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
