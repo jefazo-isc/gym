c46d935c198feb5e08364b627666ac6845cfcfc3
yahir <pouputopou4@gmail.com>
2025-05-09 11:47:23 -0600

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CardsPrincipalComponent } from '../../cards-principal/cards-principal.component';
 import { CarruselPrincipalComponent } from '../../carrusel-principal/carrusel-principal.component';
 import { UbicacionesPrincipalComponent } from '../../ubicaciones-principal/ubicaciones-principal.component';
+import { VideoComponent} from '../video/video.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardsPrincipalComponent, CarruselPrincipalComponent,UbicacionesPrincipalComponent],
+  imports: [CardsPrincipalComponent, CarruselPrincipalComponent,VideoComponent,UbicacionesPrincipalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
